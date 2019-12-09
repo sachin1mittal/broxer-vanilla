53d5d2662faf50c1863352a9b2a1f769551d5ddc
Sachin Mittal <sachinmittal33@gmail.com>
2019-12-09 12:20:05 +0530

    Stabilize seeds

--- db/seeds/002_categories.rb
+++ db/seeds/002_categories.rb
@@ -2,7 +2,7 @@ puts 'Starting Categories'
 
 10.times do
   parent_cat1 = Category.create!(
-    name: Faker::Hacker.ingverb,
+    name: Faker::Hacker.ingverb + SecureRandom.hex[0..2],
     logo: File.open('/Users/sachin/Desktop/a.jpg', 'rb'),
     description: Faker::Lorem.sentence,
     banner: File.open('/Users/sachin/Desktop/a.jpg', 'rb')
@@ -11,7 +11,7 @@ puts 'Starting Categories'
   10.times do |count|
     Category.create!(
       parent_id: parent_cat1.id,
-      name: Faker::Hacker.ingverb,
+      name: Faker::Hacker.ingverb + SecureRandom.hex[0..2],
       description: Faker::Lorem.sentence,
       logo: File.open('/Users/sachin/Desktop/a.jpg', 'rb'),
       banner: File.open('/Users/sachin/Desktop/a.jpg', 'rb')
