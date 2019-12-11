28920ecac1173f22b0a47e1f8018b75418b8819e
Sachin Mittal <sachin.mittal@vineti.com>
2019-12-12 01:08:44 +0530

    Remove authorization commented code

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,10 +3,5 @@ class AuthController < BaseController
 
   def authorize_user!
   	true
-    # AuthModule::Permissions.new(
-    #   user: current_user,
-    #   controller: params[:controller],
-    #   action: params[:action]
-    # ).authorize!
   end
 end
